30553eda926f5bd50a8c6b30a6fe8e537e9497bd
Travis Chase <travis.chase@gmail.com>
2018-01-04 18:44:56 -0700

    updating to phaser-ce 2.9.4 and removing invalid unit test

--- test/src/ui/text-buffer.js
+++ test/src/ui/text-buffer.js
@@ -11,14 +11,6 @@ describe('TextBuffer', () => {
         });
     });
 
-    describe('lineStyle()', () => {
-        it('allows properties to be overwritten', () => {
-            let style = textBuffer.lineStyle('rubik', {font: 'overwritten'});
-
-            assert.equal(style.font, 'overwritten');
-        });
-    });
-
     describe('splitTextIntoLines()', () => {
         it('splits text into multiple lines', () => {
             let ara = textBuffer.splitTextIntoLines('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam hendrerit risus venenatis porttitor molestie. Vivamus diam arcu, volutpat eu suscipit id, scelerisque eget turpis. Donec feugiat id mi nec cursus. Praesent eget sapien aliquet justo placerat consequat et sit amet metus. Praesent luctus commodo placerat.');
